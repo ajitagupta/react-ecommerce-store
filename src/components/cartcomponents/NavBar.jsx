da1caa2d93c6ae52904a59dab63017a44fe935f4
ajitagupta <g.ajita25@gmail.com>
2024-07-22 20:23:32 +0200

    Fourth commit

--- src/components/cartcomponents/NavBar.jsx
+++ src/components/cartcomponents/NavBar.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart"
-import "./NavBar.css"
+import React from 'react';
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import './NavBar.css';
 
 const NavBar = ({ setShow, size }) => {
   return (
-    <nav>
-        <div className='nav_box'>
-                <span className='my_shop'>
-
-                </span>
-                <div className='footer'>
-                 <h3 onClick={() => setShow(true)}><button className='nav_button'>MyStore</button></h3>
-                 <div className='right_topnav'>
-                    <div className="cartContainer">
-                      <ShoppingCartIcon style={{ fontSize: '24px' }} onClick={() => setShow(false)}/>
-                      <span className='shoppedItems'>{size}</span>
-                    </div>
-                 </div>
-
-                </div>
+    <nav style={{ backgroundColor: '#333', color: '#fff' }}>
+      <div className='nav_box'>
+        <span className='my_shop'></span>
+        <div className='footer'>
+          <h3 onClick={() => setShow(true)}>
+            <button className='nav_button' style={{ backgroundColor: '#444', color: '#fff' }}>MyStore</button>
+          </h3>
+          <div className='right_topnav'>
+            <div className="cartContainer">
+              <ShoppingCartIcon style={{ fontSize: '24px', color: '#fff' }} onClick={() => setShow(false)} />
+              <span className='shoppedItems' style={{ color: '#fff' }}>{size}</span>
+            </div>
+          </div>
         </div>
-
+      </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
